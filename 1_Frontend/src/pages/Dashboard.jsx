78e3274474241ad2aa4f20fef4d800877db20e39
Alain <alainmachacacahuana@gmail.com>
2026-07-20 01:11:21 -0500

    login funcioanando entrando a dasboard

---
+++ 1_Frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+function Dashboard(){
+
+    return (
+
+        <div>
+            <h1>
+                Dashboard Academia Einstein
+            </h1>
+            <p>
+                Bienvenido al sistema
+            </p>
+        </div>
+    )
+
+}
+
+export default Dashboard;
